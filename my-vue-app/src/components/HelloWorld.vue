eb0fab5877dcfe599848d3ade995d79c3aff900d
MinWook Lee <dlalsdnr0315@yuhan.ac.kr>
2025-06-13 09:23:27 +0900

    테스트

--- my-vue-app/src/components/HelloWorld.vue
+++ my-vue-app/src/components/HelloWorld.vue
@@ -2,17 +2,11 @@
 
 <template>
   <div>
-    <p>하이 테스트 중중입니다1234!!</p>
-    <p>커밋 테스트</p>
-    <p>커밋하기기</p>
-    <p>테스트2</p>
-    <p>테스트3</p>
-    <p>테스트4</p>
-    <p>테스트5</p>
-    <p>테스트5</p>
-    <P>테스트7</P>
-    <P>테스트7</P>
-    <P>잔디심기</P>
-    <P>잔디심기1</P>
+    <h1>Hello World</h1>
+    <p>Hello World</p>
+    <p>Hello World</p>
+    <p>Hello World</p>
+    <p>Hello World</p>
+    <p>Hello World</p>
   </div>
 </template>
