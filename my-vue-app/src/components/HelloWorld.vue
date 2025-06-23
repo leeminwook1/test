026413751d80ac5a796b83fb1e7358dadab6336b
MinWook Lee <dlalsdnr0315@yuhan.ac.kr>
2025-06-23 13:06:42 +0900

    롤백

--- my-vue-app/src/components/HelloWorld.vue
+++ my-vue-app/src/components/HelloWorld.vue
@@ -3,18 +3,5 @@
 <template>
   <div>
     <h1>Hello World</h1>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
   </div>
 </template>
